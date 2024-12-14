1806864a80a5bd67a8d3d139e84bd204cc89aca8
destroSunRay <95708080+destroSunRay@users.noreply.github.com>
2024-12-14 15:26:43 -0500

    part-2, introdemo, 2.1-2.5

---
+++ part-2/introdemo/src/components/Total.jsx
@@ -0,0 +1,12 @@
+const Total = ({ parts }) => {
+  return (
+    <p>
+      <b>
+        total of {parts.reduce((acc, part) => acc + part.exercises, 0)}{" "}
+        exercises
+      </b>
+    </p>
+  );
+};
+
+export default Total;
