b38e52930138cf0440f42383c8c2b422bf9a1c0f
destroSunRay <95708080+destroSunRay@users.noreply.github.com>
2024-12-21 12:52:52 -0500

    part-4, exercises 4.8-4.12

--- part-4/blogs/controllers/blogs.js
+++ part-4/blogs/controllers/blogs.js
@@ -2,22 +2,22 @@ const Blog = require('../models/blogs')
 const blogsRouter = require('express').Router()
 
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response, next) => {
+  try {
+    const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+    const result = await blog.save()
+    response.status(201).json(result)
+  } catch (error) {
+    response.status(400).send(error)
+    next(error)
+  }
+
 })
 
 module.exports = blogsRouter
---
+++ part-4/blogs/test/blogs_api.test.js
@@ -0,0 +1,90 @@
+const { test, after, beforeEach, describe } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blogs')
+
+const api = supertest(app)
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('blog has an unique identifier id as string', async () => {
+  const blog = {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "ajhbskn",
+    likes: 12
+  }
+
+  const response = await api.post('/api/blogs').send(blog)
+
+  assert(response.body.hasOwnProperty('id'));
+
+})
+
+test('verifed post is appending blog to the blogs list', async () => {
+  const intialBlogs = await api.get('/api/blogs')
+  const initalBlogsLength = intialBlogs.body.length
+
+  const blog = {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "kasndkln",
+    likes: 12
+  }
+
+  const response = await api.post('/api/blogs').send(blog)
+  response.body
+  const blogsAfterAppending = await api.get('/api/blogs')
+
+  const currentBlogsLength = blogsAfterAppending.body.length
+  assert.strictEqual(currentBlogsLength, initalBlogsLength + 1)
+})
+
+test('verifies that if the likes property is missing from the request, it will default to the value 0', async () => {
+  const blog = {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "ajksbd.com/blogs"
+  }
+
+  const response = await api.post('/api/blogs').send(blog)
+  assert.strictEqual(response.body.likes, 0)
+})
+
+describe('verify that if the title or url properties are missing from the request data, the backend responds to the request with the status code 400 Bad Request', () => {
+  test('missing title in post object results in 400 Bad Request', async () => {
+    const blog = {
+      author: "Edsger W. Dijkstra",
+      url: "ajksbd.com/blogs"
+    }
+
+    const response = await (await api.post('/api/blogs').send(blog))
+    assert.strictEqual(response.status, 400)
+  })
+
+  test('missing url in post object results in 400 Bad Request', async () => {
+    const blog = {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+    }
+
+    const response = await (await api.post('/api/blogs').send(blog))
+    assert.strictEqual(response.status, 400)
+  })
+})
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+})
+
+
+after(async () => {
+  await mongoose.connection.close()
+})
